f511678469a62eea7edbf801305f371ca7e0f0cb
SWAPNIL <swa2314@gmail.com>
2025-05-25 23:53:02 -0400

    removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from rich.console import Console
-import time
-
-console = Console()
-with console.status("[bold green]Thinking...", spinner="toggle2"):
-    time.sleep(3)
-console.print("[bold green]Done![/]")
